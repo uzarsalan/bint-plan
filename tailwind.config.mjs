6a7dd89d2eaa2c8f29f09bc6e7883e4c3e921b49
Arsalan <uzarsalan@gmail.com>
2026-05-27 12:07:40 +0300

    Initial commit: bint.ru plan v2 dashboard

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx,vue,svelte}',
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
